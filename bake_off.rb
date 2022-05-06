6066ca8dc623f0c0b1fa9a3b70afc4de609239c5
willianchu <willianchu@hotmail.com>
2022-05-06 15:42:11 -0700

    cross  search array of hashes

--- bake_off.rb
+++ bake_off.rb
@@ -1,41 +1,56 @@
-# const chooseRecipe = function(bakeryA, bakeryB, recipes) {
-#   // Code here!
-# }
+def chooseRecipe(bakeryA, bakeryB, recipes)
+  found = false
+  recipe_name = ""
+  recipes.each do |recipe|
+    recipe["ingredients"].each do |ingredient|
+      if ingrient.include?(bakeryA) || ingrient.include?(bakeryB)
+        found = true
+        recipe_name = recipe["name"]
+      else
+        found = false
 
-# let bakeryA = ['saffron', 'eggs', 'tomato paste', 'coconut', 'custard'];
-# let bakeryB = ['milk', 'butter', 'cream cheese'];
-# let recipes = [
-#     {
-#         name: 'Coconut Sponge Cake',
-#         ingredients: ['coconut', 'cake base']
-#     },
-#     {
-#         name: 'Persian Cheesecake',
-#         ingredients: ['saffron', 'cream cheese']
-#     },
-#     {
-#         name: 'Custard Surprise',
-#         ingredients: ['custard', 'ground beef']
-#     }
-# ];
+      end
+    end
+  end
+  recipe_name
+end
 
-# console.log(chooseRecipe(bakeryA, bakeryB, recipes));
 
-# bakeryA = ['potatoes', 'bay leaf', 'raisins'];
-# bakeryB = ['red bean', 'dijon mustard', 'apples'];
-# recipes = [
-#     {
-#         name: 'Potato Ganache',
-#         ingredients: ['potatoes', 'chocolate']
-#     },
-#     {
-#         name: 'Sweet Fish',
-#         ingredients: ['anchovies', 'honey']
-#     },
-#     {
-#         name: "Nima's Famous Dijon Raisins",
-#         ingredients: ['dijon mustard', 'raisins']
-#     }
-# ];
+bakeryA = ['saffron', 'eggs', 'tomato paste', 'coconut', 'custard']
+bakeryB = ['milk', 'butter', 'cream cheese']
+#list of hashes
+recipes = [
+  { 
+    "name": "Coconut Sponge Cake",
+    "ingredients": ["coconut", "cake base"]
+  },
+  {
+    "name": "Persian Cheesecake",
+    "ingrients": ["saffron", "cream cheese"]
+  },
+  {
+    "name": "Custard Surprise",
+    "ingredients": ["custard", "ground beef"]
+  }
+]
 
-# console.log(chooseRecipe(bakeryA, bakeryB, recipes));
+puts chooseRecipe(bakeryA, bakeryB, recipes)
+
+bakeryA = ['potatoes', 'bay leaf', 'raisins']
+bakeryB = ['red bean', 'dijon mustard', 'apples']
+
+recipes = [
+  {
+    "name": "Potato Ganache",
+    "ingredients": ["potatoes", "chocolate"]
+  },
+  {
+    "name": "Sweet Fish",
+  },
+  {
+    "name": "Nima's Famous Dijon Raisins",
+    "ingredients": ["dijon mustard", "raisins"]
+  }
+]
+
+puts chooseRecipe(bakeryA, bakeryB, recipes)
