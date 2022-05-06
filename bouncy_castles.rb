c17ffdd7f1d70a8dadd53b7c25cc3375bc917627
willianchu <willianchu@hotmail.com>
2022-05-05 22:23:09 -0700

    input in javascript to solve in Ruby

---
+++ bouncy_castles.rb
@@ -0,0 +1,48 @@
+# // Use the value below whenever you need the value of Pi
+# const PI = 3.14159 ;
+
+# const sphereVolume = function (radius) {
+#   // Code here!
+# }
+
+# console.log(4186 < sphereVolume(10) && sphereVolume(10) < 4189);
+
+# const coneVolume = function (radius, height) {
+#   // And here!
+# }
+
+# console.log(45 < coneVolume(3, 5) && coneVolume(3, 5) < 49);
+
+# const prismVolume = function (height, width, depth) {
+#   // Probably here too!
+# }
+
+# console.log(prismVolume(3, 4, 5) === 60);
+
+# const totalVolume = function (solids) {
+#   // Code here? Yup!
+# }
+
+# const largeSphere = {
+#   type: 'sphere',
+#   radius: 40
+# }
+
+# const smallSphere = {
+#   type: 'sphere',
+#   radius: 10
+# }
+
+# const cone = {
+#   type: 'cone',
+#   radius: 3,
+#   height: 5
+# }
+
+# const duck = [
+#   largeSphere,
+#   smallSphere,
+#   cone
+# ]
+
+# console.log(272000 < totalVolume(duck) && totalVolume(duck) < 275000);
