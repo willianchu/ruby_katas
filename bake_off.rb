3f2d1ec7bb2ef2e78808ad2cfe0028a5dbd53359
willianchu <willianchu@hotmail.com>
2022-05-06 15:22:24 -0700

    javascript input to solve in ruby

---
+++ bake_off.rb
@@ -0,0 +1,41 @@
+# const chooseRecipe = function(bakeryA, bakeryB, recipes) {
+#   // Code here!
+# }
+
+# let bakeryA = ['saffron', 'eggs', 'tomato paste', 'coconut', 'custard'];
+# let bakeryB = ['milk', 'butter', 'cream cheese'];
+# let recipes = [
+#     {
+#         name: 'Coconut Sponge Cake',
+#         ingredients: ['coconut', 'cake base']
+#     },
+#     {
+#         name: 'Persian Cheesecake',
+#         ingredients: ['saffron', 'cream cheese']
+#     },
+#     {
+#         name: 'Custard Surprise',
+#         ingredients: ['custard', 'ground beef']
+#     }
+# ];
+
+# console.log(chooseRecipe(bakeryA, bakeryB, recipes));
+
+# bakeryA = ['potatoes', 'bay leaf', 'raisins'];
+# bakeryB = ['red bean', 'dijon mustard', 'apples'];
+# recipes = [
+#     {
+#         name: 'Potato Ganache',
+#         ingredients: ['potatoes', 'chocolate']
+#     },
+#     {
+#         name: 'Sweet Fish',
+#         ingredients: ['anchovies', 'honey']
+#     },
+#     {
+#         name: "Nima's Famous Dijon Raisins",
+#         ingredients: ['dijon mustard', 'raisins']
+#     }
+# ];
+
+# console.log(chooseRecipe(bakeryA, bakeryB, recipes));
