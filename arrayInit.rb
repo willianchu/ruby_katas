2137594abcd3f0bdb7f0c4aab681593dbfd9e940
willianchu <willianchu@hotmail.com>
2022-07-07 15:42:40 -0700

    lenght from a initial osition in array

---
+++ arrayInit.rb
@@ -0,0 +1,23 @@
+# Initialize an empty array with the variable name array
+# Hint
+
+array = Array.new
+# or
+
+array = []
+
+# Initialize an array with exactly one nil element in it with the variable name array_1
+# Hint
+
+array_1 = Array.new(1)
+# or
+
+array_1 = [nil]
+
+# Initialize an array with exactly two elements with value 10 in it using the variable name array_2.
+# Hint
+
+array_2 = Array.new(2, 10)
+# or
+
+array_2 = [10, 10]
