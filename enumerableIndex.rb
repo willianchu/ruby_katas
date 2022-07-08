ff0c955632a686721c79aca3b4693abe128f86b3
willianchu <willianchu@hotmail.com>
2022-07-08 16:54:22 -0700

    delete first with drop instead shift

--- enumerableIndex.rb
+++ enumerableIndex.rb
@@ -17,10 +17,21 @@ skip_animals(['leopard', 'bear', 'fox', 'wolf'], 2)
 # =["2:fox", "3:wolf"]
 # It is guaranteed that number of items in animals array is greater than the value of skip.
 
-# optimal
-
-
-# solution 1
+# first try
 def skip_animals(animals, skip)
-  animals.each_with_index { |item, index| p "#{index}:#{item}" if index >= skip }
+  animals.map.with_index { |v, i| "#{i}:#{v}" }.drop(skip) # drop instead shift
+end
+
+# solution 1.1
+def skip_animals(animals, skip)
+  animals.map.with_index{|a, index| "#{index}:#{a}" if index >= skip}.compact #compact removes nil values
+end
+
+#similar
+def skip_animals(animals, skip)
+  results = []
+  animals.drop(skip).each_with_index do |animal, i|
+      results << "#{i+skip}:#{animal}"
+  end
+  results
 end
