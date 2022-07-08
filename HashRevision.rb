e964a60020849f567bf98f295c29bc88bae30742
willianchu <willianchu@hotmail.com>
2022-07-08 10:13:39 -0700

    destructive action

--- HashRevision.rb
+++ HashRevision.rb
@@ -35,3 +35,13 @@ h.keep_if {|key, value| key % 2 == 0}
 # A key-value pair [543121, 100] to the hackerrank object using store
 # Retain all key-value pairs where keys are Integers ( clue : is_a? Integer )
 # Delete all key-value pairs where keys are even-valued.
+
+hackerrank.store(543121, 100)
+
+hackerrank.keep_if {|key, value| key % 1 == 0}
+# or
+hackerrank.keep_if{|key,value| key.is_a?Integer}
+
+hackerrank.delete_if {|key, value| key % 2 == 0}
+
+
