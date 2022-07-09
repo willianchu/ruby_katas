66403986cfd98fab3ad83213e93e6781e78cd54d
willianchu <willianchu@hotmail.com>
2022-07-08 18:26:00 -0700

    cypher13 ROT13

--- enumerablesCollect.rb
+++ enumerablesCollect.rb
@@ -1,28 +1,54 @@
-Beside simple methods to iterate over objects, Ruby Enumerable provides a number of important higher order constructs that we shall explore in further challenges. One of such important methods is collect method, also known as map.
+# map
 
-map as the name may suggest, takes a function and maps (applies) it to a collection of values one by one and returns the collection of result.
+[1,2,3].map { |x| 2*x }
+# => [2, 4, 6]
 
-That is,
+{:a=>1, :b=>2, :c=>3}.collect { |key, value| 2*value }
+# => [2, 4, 6]
 
-This single powerful method helps us to operate on a large number of values at once.
+# Note that these methods are different from each in the respect that these methods return a new collection while former returns the original collection, irrespective of whatever happens inside the block.
 
-For example,
+#Write a method which takes an array of strings (containing secret enemy message bits!) and decodes its elements using ROT13 cipher system; returning an array containing the final messages.
 
->>> [1,2,3].map { |x| 2*x }
-=> [2, 4, 6]
->>> {:a=>1, :b=>2, :c=>3}.collect { |key, value| 2*value }
-=> [2, 4, 6]
-Note that these methods are different from each in the respect that these methods return a new collection while former returns the original collection, irrespective of whatever happens inside the block.
+# For example, this is how ROT13 algorithm works,
 
-In this challenge, your task is to write a method which takes an array of strings (containing secret enemy message bits!) and decodes its elements using ROT13 cipher system; returning an array containing the final messages.
+# Original text:
 
-For example, this is how ROT13 algorithm works,
+# Why did the chicken cross the road?
+# Gb trg gb gur bgure fvqr!
 
-Original text:
+# On application of ROT13,
 
-Why did the chicken cross the road?
-Gb trg gb gur bgure fvqr!
-On application of ROT13,
+# Jul qvq gur puvpxra pebff gur ebnq?
+# To get to the other side!
 
-Jul qvq gur puvpxra pebff gur ebnq?
-To get to the other side!
+# REGEX solution
+def rot13(secret_messages)
+  secret_messages.map { |c| c.tr("a-z", "n-za-m") }
+end
+def rot13(secret_messages)
+   secret_messages.map {|i| i.tr("n-za-m","a-z") }
+end
+
+# OR
+def cypher(entry)
+  chars = entry.split('')
+  chars.map { |x| 
+      if (x.ord >= "A".ord) && (x.ord <= "M".ord) then 
+          (x.ord+13).chr  
+      elsif (x.ord >= "N".ord) && (x.ord <= "Z".ord) then 
+          (x.ord-13).chr 
+      elsif (x.ord >= "a".ord) && (x.ord <= "m".ord) then 
+          (x.ord+13).chr  
+      elsif (x.ord >= "n".ord) && (x.ord <= "z".ord) then 
+          (x.ord-13).chr
+      else 
+          x 
+      end
+    }.join('')
+end
+          
+def rot13(secret_messages) 
+    secret_messages.each {|phrase| cypher(phrase)}
+end
+ 
