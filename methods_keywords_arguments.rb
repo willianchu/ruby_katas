b4b2b023b43c7293e66a4a2f795573909d9f60c0
willianchu <willianchu@hotmail.com>
2022-07-18 15:22:27 -0700

    hash as argument

--- methods_keywords_arguments.rb
+++ methods_keywords_arguments.rb
@@ -35,3 +35,17 @@ convert_temp(0, input_scale: 'celsius', output_scale: 'kelvin')
 convert_temp(0, input_scale: 'celsius', output_scale: 'fahrenheit')
 # => 32.0
 # Note that the input values are lowercase version of corresponding scales.
+
+def convert_temp(temp, input_scale:, output_scale: 'Celsius')
+  case input_scale.downcase
+  when 'fahrenheit'
+      return output_scale == 'kelvin' ? 
+      ((temp - 32) / 1.8) + 273.15 : (temp - 32) / 1.8
+  when 'celsius'
+      return output_scale == 'kelvin' ? 
+      temp + 273.15 : (temp * 1.8) + 32
+  when 'kelvin'
+      return output_scale == 'celsius' ? 
+      temp - 273.15 : (temp - 273.15) * 1.8
+  end
+end
