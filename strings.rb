fee5dbb6f11a0da130720d4e01d759d873c26864
willianchu <willianchu@hotmail.com>
2022-07-20 21:37:39 -0700

    string quotes examples

---
+++ strings.rb
@@ -0,0 +1,13 @@
+def single_quote
+  'Hello World and others!'
+  end
+  
+  def double_quote
+  "Hello World and others!"
+  end
+  
+  def here_doc
+  <<-MY_HEREDOC
+  Hello World and others!
+  MY_HEREDOC
+  end
