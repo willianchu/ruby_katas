ad5ca0c75e8b1d47ac198ffe2ce80830a800578b
willianchu <willianchu@hotmail.com>
2022-07-24 22:24:33 -0700

    set problem

---
+++ simpleSum.rb
@@ -0,0 +1,29 @@
+#!/bin/ruby
+
+require 'json'
+require 'stringio'
+
+#
+# Complete the 'simpleArraySum' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY ar as parameter.
+#
+
+def simpleArraySum(ar)
+    # Write your code here
+
+end
+
+fptr = File.open(ENV['OUTPUT_PATH'], 'w')
+
+ar_count = gets.strip.to_i
+
+ar = gets.rstrip.split.map(&:to_i)
+
+result = simpleArraySum ar
+
+fptr.write result
+fptr.write "\n"
+
+fptr.close()
