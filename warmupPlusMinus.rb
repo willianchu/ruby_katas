36fe1513509471791e5a6b6d9d3d9706ef62ab03
willianchu <willianchu@hotmail.com>
2022-07-25 11:20:41 -0700

    set problem

---
+++ warmupPlusMinus.rb
@@ -0,0 +1,21 @@
+#!/bin/ruby
+
+require 'json'
+require 'stringio'
+
+#
+# Complete the 'plusMinus' function below.
+#
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def plusMinus(arr)
+    # Write your code here
+
+end
+
+n = gets.strip.to_i
+
+arr = gets.rstrip.split.map(&:to_i)
+
+plusMinus arr
