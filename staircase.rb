9e3a46677642a0c391cfb650597f30d016c7665d
willianchu <willianchu@hotmail.com>
2022-08-01 12:15:00 -0700

    iptimizing

---
+++ staircase.rb
@@ -0,0 +1,42 @@
+#!/bin/ruby
+
+require 'json'
+require 'stringio'
+
+#
+# Complete the 'staircase' function below.
+#
+# The function accepts INTEGER n as parameter.
+#
+def repeatChar (symbol, number)
+    result = ""
+    number.times { result += symbol }
+    result
+end
+
+def staircase(n)
+    for i in 1..n
+        spaces = repeatChar(' ', (n - i))
+        blocks = repeatChar('#', i)
+        puts "%s%s" % [spaces, blocks]
+    end
+
+end
+
+
+n = gets.strip.to_i
+
+staircase n
+
+
+# Version 2
+
+def stair(spaces, hashes)
+  " " * spaces + "#" * hashes
+end
+
+n = gets.to_i
+
+for i in 1..n do
+  puts stair(n - i, i)
+end
