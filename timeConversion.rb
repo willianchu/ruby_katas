0f121cba0529145c6bf862ee72e9c03139e5a961
willianchu <willianchu@hotmail.com>
2022-08-03 08:19:23 -0700

    returning 00 hour to 24hs

--- timeConversion.rb
+++ timeConversion.rb
@@ -12,11 +12,15 @@ require 'stringio'
 
 def timeConversion(s)
   am_pm = s[-2..-1]
+  puts am_pm
   hour = s[0..1].to_i
   if am_pm == "PM"
     hour = hour + 12
+    hour = hour == 24 ? "00" : hour
   end
+  puts hour
   return hour.to_s + s[2..-3]
+
 end
 
 fptr = File.open(ENV['OUTPUT_PATH'], 'w')
