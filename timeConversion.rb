2720a8622fe17d638c0fb2e5e120c22a316bbc19
willianchu <willianchu@hotmail.com>
2022-08-03 08:31:48 -0700

    checks begining of the day

--- timeConversion.rb
+++ timeConversion.rb
@@ -14,9 +14,11 @@ def timeConversion(s)
   am_pm = s[-2..-1]
   puts am_pm
   hour = s[0..1].to_i
-  if am_pm == "PM"
+  if am_pm == "PM" || am_pm == "pm"
     hour = hour + 12
     hour = hour == 24 ? "00" : hour
+  elsif am_pm == "AM" && hour == 12
+    hour = "00"
   end
   puts hour
   return hour.to_s + s[2..-3]
