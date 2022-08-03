f405c699f2504680c4260a5b89e2066602dbc278
willianchu <willianchu@hotmail.com>
2022-08-03 07:58:10 -0700

    set problem

---
+++ timeConversion.rb
@@ -0,0 +1,27 @@
+#!/bin/ruby
+
+require 'json'
+require 'stringio'
+
+#
+# Complete the 'timeConversion' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def timeConversion(s)
+    # Write your code here
+
+end
+
+fptr = File.open(ENV['OUTPUT_PATH'], 'w')
+
+s = gets.chomp
+
+result = timeConversion s
+
+fptr.write result
+fptr.write "\n"
+
+fptr.close()
