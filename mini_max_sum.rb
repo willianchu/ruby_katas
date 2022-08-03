8a78ecd2d6def426e8f3ebce38b8ff062ec49dae
willianchu <willianchu@hotmail.com>
2022-08-02 22:48:15 -0700

    for loop

--- mini_max_sum.rb
+++ mini_max_sum.rb
@@ -11,11 +11,13 @@ require 'stringio'
 
 def miniMaxSum(arr)
   minSum = 0
-  0.step{|i| minSum = minSum + arr[i].to_i; break if i > arr.size-1}
+  for i in 0..arr.length-1
+    minSum = minSum + arr[i].to_i;  
+  end
+  
   maxSum = 0
   1.step{|i| maxSum = maxSum + arr[i].to_i; break if i > arr.size}
   puts "#{minSum} #{maxSum}"
-0.step{|i| puts i ; break if i>100 }
 end
 
 arr = gets.rstrip.split.map(&:to_i)
