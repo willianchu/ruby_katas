93e5214c550e3218cef23bdc604b1a73041a177a
willianchu <willianchu@hotmail.com>
2022-08-02 22:15:22 -0700

    set problem

---
+++ mini_max_sum.rb
@@ -0,0 +1,19 @@
+#!/bin/ruby
+
+require 'json'
+require 'stringio'
+
+#
+# Complete the 'miniMaxSum' function below.
+#
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def miniMaxSum(arr)
+    # Write your code here
+
+end
+
+arr = gets.rstrip.split.map(&:to_i)
+
+miniMaxSum arr
