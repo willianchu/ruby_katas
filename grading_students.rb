1c531753c87dc67bb5758d50759da5703ade4d62
willianchu <willianchu@hotmail.com>
2022-08-04 22:09:52 -0700

    set problem

---
+++ grading_students.rb
@@ -0,0 +1,34 @@
+#!/bin/ruby
+
+require 'json'
+require 'stringio'
+
+#
+# Complete the 'gradingStudents' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY grades as parameter.
+#
+
+def gradingStudents(grades)
+    # Write your code here
+
+end
+
+fptr = File.open(ENV['OUTPUT_PATH'], 'w')
+
+grades_count = gets.strip.to_i
+
+grades = Array.new(grades_count)
+
+grades_count.times do |i|
+    grades_item = gets.strip.to_i
+    grades[i] = grades_item
+end
+
+result = gradingStudents grades
+
+fptr.write result.join "\n"
+fptr.write "\n"
+
+fptr.close()
