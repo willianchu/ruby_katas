0982369b756f215ff75b0c6f2819087f6d126e9e
willianchu <willianchu@hotmail.com>
2022-08-11 18:54:41 -0700

    solved refactoring

--- grading_students.rb
+++ grading_students.rb
@@ -1,34 +1,15 @@
-#!/bin/ruby
-
-require 'json'
-require 'stringio'
-
-#
-# Complete the 'gradingStudents' function below.
-#
-# The function is expected to return an INTEGER_ARRAY.
-# The function accepts INTEGER_ARRAY grades as parameter.
-#
-
 def gradingStudents(grades)
-    # Write your code here
-
+  newGrades = []
+  grades.each do |grade|
+    if grade < 38
+      newGrades.push grade
+    else
+      if grade % 5 < 3
+        newGrades.push grade
+      else
+        newGrades.push grade + (5 - grade % 5)
+      end
+    end
+  end
+  newGrades
 end
-
-fptr = File.open(ENV['OUTPUT_PATH'], 'w')
-
-grades_count = gets.strip.to_i
-
-grades = Array.new(grades_count)
-
-grades_count.times do |i|
-    grades_item = gets.strip.to_i
-    grades[i] = grades_item
-end
-
-result = gradingStudents grades
-
-fptr.write result.join "\n"
-fptr.write "\n"
-
-fptr.close()
