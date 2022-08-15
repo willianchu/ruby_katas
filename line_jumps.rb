5eba436909ceb4cfdf36833660448eb91d22eee9
willianchu <willianchu@hotmail.com>
2022-08-15 09:20:11 -0700

    50% of cases

---
+++ line_jumps.rb
@@ -0,0 +1,16 @@
+# Complete the 'kangaroo' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. INTEGER x1 - starting position of kangaroo 1
+#  2. INTEGER v1 - jump distance of kangaroo 1
+#  3. INTEGER x2 - starting position of kangaroo 2
+#  4. INTEGER v2 - jump distance of kangaroo 2
+#
+
+def kangaroo(x1, v1, x2, v2) # solve 50% of cases
+  jump = (x1 - x2) / (v2 - v1)
+  puts jump
+  jump > 0 && jump.integer? ? "YES" : "NO"
+end
+
