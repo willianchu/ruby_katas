c9b9071df787fbcb1271f8e1485bd3a4a96a2987
willianchu <willianchu@hotmail.com>
2022-08-16 13:55:50 -0700

    it was a string path

--- counting_valleys.rb
+++ counting_valleys.rb
@@ -1,10 +1,11 @@
 def countingValleys(steps, path)
   count_valleys = 0
   sea_level = 0
-  path.each { |step|
+  arr = path.chars
+  arr.each { |step|
       if step == "U"
           sea_level += 1
-          if sea_level = 0
+          if sea_level == 0
               count_valleys += 1
           end
       elsif step == "D"
