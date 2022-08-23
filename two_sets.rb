bd8f3ad5c280e8e8bacf5f7918a148543c5fb1bf
willianchu <willianchu@hotmail.com>
2022-08-22 21:37:49 -0700

    ultimate version

--- two_sets.rb
+++ two_sets.rb
@@ -6,32 +6,29 @@
 #  1. INTEGER_ARRAY a
 #  2. INTEGER_ARRAY b
 #
-def is_divisible (num, arr)
-  arr.each do |element|
-    return false if (num % element)
-  end
-  return true
-end
 def getTotalX(a, b)
-    first_number = a.min()
-    # strange the for loop doesnt count to 100 I have to add +1
-    last_number = b.max() + 1
-    all_match_array = []
-    for i in first_number...last_number
-        all_divisible = is_divisible(i, a)
-        if all_divisible
-            all_match_array.push(i)
-        end
-    end
-    answer_array = []
-    result = 0
-    all_match_array.each do |number|
-        all_divisible = is_divisible(number, b)
-        if all_divisible == true
-            answer_array.push(number)
-        end
-    end
-    answer_array.length()
+  all_match_array = []
+  range = (a.min()..b.max()).to_a
+  range.each do |i| 
+      all_divisible = true
+      a.each do |element|
+          if ( i % element) != 0
+              all_divisible = false
+              break
+          end
+      end
+      if all_divisible
+          b.each do |element|
+              result = element % i
+              if (result != 0)
+                  all_divisible = false
+                  break
+              end    
+          end
+          all_match_array.push(i) if all_divisible == true
+      end
+  end
+  all_match_array.length()
 end
 
 puts getTotalX([1], [100])
