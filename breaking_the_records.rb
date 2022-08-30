3b6e8cde7fe12f121741a1f72f71c98ecbb624be
willianchu <willianchu@hotmail.com>
2022-08-29 18:57:40 -0700

    solved 4 variables

--- breaking_the_records.rb
+++ breaking_the_records.rb
@@ -11,9 +11,24 @@ require 'stringio'
 #
 
 def breakingRecords(scores)
-    # Write your code here
-
+    maximum = scores [0]
+    minimum = scores[0]
+    min_count = 0
+    max_count = 0
+    scores.each do |score|
+        if score > maximum
+            maximum = score
+            max_count += 1
+        elsif score < minimum
+            minimum = score
+            min_count += 1
+        end
+    end
+    [max_count, min_count]
 end
+
+
+
 
 fptr = File.open(ENV['OUTPUT_PATH'], 'w')
 
